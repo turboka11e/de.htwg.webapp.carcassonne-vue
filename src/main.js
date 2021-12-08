401d21f78f55243696b2bf5dcd1f0b4964895b50
turbokalle <lukas.kaluscha@gmail.com>
2021-12-08 09:25:48 +0100

    implemented pwa

--- src/main.js
+++ src/main.js
@@ -7,6 +7,21 @@ import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 import 'vuetify/dist/vuetify.min.css'
 import vuetify from "@/plugins/vuetify";
+import './registerServiceWorker'
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(async function() {
+    try{
+      var res = await fetch(event.request);
+      var cache = await caches.open('cache');
+      cache.put(event.request.url, res.clone());
+      return res;
+    }
+    catch(error){
+      return caches.match(event.request);
+    }
+  }());
+});
 
 Vue.config.productionTip = false
 
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,32 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    https: true
+  }
+  // pwa: {
+  //   name: 'Carcassonne',
+  //   short_name: 'Carcassonne',
+  //   start_url: "index.html",
+  //   display: "standalone",
+  //   background_color: '#000000',
+  //   themeColor: '#4DBA87',
+  //   msTileColor: '#000000',
+  //   appleMobileWebAppCapable: 'yes',
+  //   appleMobileWebAppStatusBarStyle: 'black',
+  //   manifestOptions: {
+  //     background_color: "#42b983"
+  //   },
+  //   icons: [
+  //
+  //   ]
+    // configure the workbox plugin
+    // workboxPluginMode: 'InjectManifest',
+    // workboxOptions: {
+    //   // swSrc is required in InjectManifest mode.
+    //   swSrc: 'service-worker.js',
+    //   // ...other Workbox options...
+    // }
+  // }
 }
